f7f7b67dc69ffc9951b722b38720e5d0fd144faa
vigarbuaa <vigarxueer@126.com>
2019-12-23 22:58:49 +0800

    add pyqt tab example

--- examples/qdark_style_example/dark_hello.py
+++ examples/qdark_style_example/dark_hello.py
@@ -3,12 +3,62 @@ import warnings
 import sys
 import qdarkstyle
 from PyQt5 import QtWidgets
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import QApplication,QMainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore  import *
 import requests
+
+class TabDemo(QTabWidget):
+
+    def __init__(self,parent=None):
+        super(TabDemo,self).__init__(parent)
+        self.tab1=QWidget()
+        self.tab2=QWidget()
+        self.tab3=QWidget()
+        self.addTab(self.tab1,"Tab1")
+        self.addTab(self.tab2,"Tab2")
+        self.addTab(self.tab3,"Tab3")
+        self.tab1UI()
+        self.tab2UI()
+        self.tab3UI()
+        self.setWindowTitle("Tab example")
+
+    def tab1UI(self):
+        layout = QFormLayout()
+        layout.addRow("姓名",QLineEdit())
+        layout.addRow("地址",QLineEdit())
+        self.setTabText(0,"联系方式")#?
+        self.tab1.setLayout(layout)
+    
+    def tab2UI(self):
+        layout = QFormLayout()
+        sex=QHBoxLayout()
+        sex.addWidget(QRadioButton("男"))
+        sex.addWidget(QRadioButton("女"))
+        layout.addRow(QLabel("性别"),sex)
+        layout.addRow("生日",QLineEdit())
+        self.setTabText(1,"个人详细信息")
+        self.tab2.setLayout(layout)
+
+    def tab3UI(self):
+        layout=QHBoxLayout()
+        layout.addWidget(QLabel("科目"))
+        layout.addWidget(QCheckBox("物理"))
+        layout.addWidget(QCheckBox("高数"))
+        self.setTabText(2,"教育程度")
+        self.tab3.setLayout(layout)
+
+class WebView(QWebEngineView):
+    def __init__(self,parent=None):
+        super(WebView,self).__init__()
+        url='http://www.ifeng.com'
+        self.load(QUrl(url))
+        self.show()
+        QTimer.singleShot(1000*10,self.close)
+
 
 # add Dialog Example to check Combox/SpinBox/connect signal
 class InterestCalc(QDialog):
@@ -142,7 +192,10 @@ class MainWindow(QMainWindow):
 if __name__=="__main__":
 # create the application and the main window
     app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
+    # window = MainWindow()
+    # window = InterestCalc()
+    #window=WebView()
+    window=TabDemo()
 
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
 
