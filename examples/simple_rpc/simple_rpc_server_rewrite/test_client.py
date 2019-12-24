ddab64027745d705c8472be7eea4f1876c476486
vigarbuaa <vigarxueer@126.com>
2019-12-24 09:39:41 +0800

    rewrite rpc server
    [study用]把rpc server单独扒出来，当成一个模块。

---
+++ examples/simple_rpc/simple_rpc_server_rewrite/test_client.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from time import sleep
+import os
+import random
+from rpc_server import RpcClient
+
+class TestClient(RpcClient):
+    """
+    Test RpcClient
+    """
+    client_name = ""
+    def __init__(self):
+        """
+        Constructor
+        """
+        super(TestClient, self).__init__()
+        self.client_name=os.getpid()
+
+    def callback(self, topic, data):
+        """
+        Realize callable function
+        """
+        print(f"client {self.client_name} received topic:{topic}, data:{data}")
+
+
+if __name__ == "__main__":
+    req_address = "tcp://localhost:2019"
+    sub_address = "tcp://localhost:9102"
+
+    tc = TestClient()
+    tc.subscribe_topic("")
+    tc.start(req_address, sub_address)
+
+    while 1:
+        print(tc.add(random.randint(1,100), random.randint(1,200)))
+        sleep(2)
+        print(tc.sub(random.randint(1,100), random.randint(1,200)))
+        sleep(2)
---
+++ examples/simple_rpc/simple_rpc_server_rewrite/test_server.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from time import sleep, time
+
+from rpc_server import RpcServer
+
+class TestServer(RpcServer):
+    """
+    Test RpcServer
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        super(TestServer, self).__init__()
+
+        self.register(self.add)
+        self.register(self.sub)
+
+    def add(self, a, b):
+        print(f"receiving:{a} {b}")
+        return a + b
+
+    def sub(self, a, b):
+        print(f"receiving:{a} {b} to sub")
+        return a - b
+
+if __name__ == "__main__":
+    rep_address = "tcp://*:2019"
+    pub_address = "tcp://*:9102"
+
+    ts = TestServer()
+    ts.start(rep_address, pub_address)
+
+    while 1:
+        content = f"current server time is {time()}"
+        print(content)
+        ts.publish("heartbeat", content)
+        sleep(2)
