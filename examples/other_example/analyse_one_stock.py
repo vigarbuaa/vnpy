cb28e839f45c3d11372482858985398c71e2e42c
qufw <qufw@htdadao.net>
2020-10-29 12:48:39 +0800

    Update analyse_one_stock.py

--- examples/other_example/analyse_one_stock.py
+++ examples/other_example/analyse_one_stock.py
@@ -4,7 +4,6 @@ import os, time, json, sys, traceback, logging, getopt
 import baostock as bs
 import pandas as pd
 
-
 def get_stock_df(stock_num):
   # rs = bs.query_history_k_data_plus(stock,"date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,isST",
   rs = bs.query_history_k_data_plus(stock,"date,code,open,high,low,close,volume,amount",
@@ -21,22 +20,28 @@ def get_stock_df(stock_num):
   result = pd.DataFrame(data_list, columns=rs.fields)
   result.to_csv(stock+"_k_data.csv", index=False)
   df = pd.read_csv(stock+"_k_data.csv",header=0,index_col=0)
-  print(df.tail(3))
+#   print(df.tail(3))
   
   macd, signal, hist = talib.MACD(df.close,fastperiod=12,slowperiod=26,signalperiod=9)
   df['macd']=macd
   df['signal']=signal
   df['hist']=hist
   print(df.tail(3))
+  return df
   
   #### 结果集输出到csv文件 ####   
   # print(result)
   
-stock="sh.600585"
+stock_list=["sh.600585","sh.600009"]
+# stock="sh.600585"
 #### 登陆系统 ####
 lg = bs.login()
 print('login respond error_code:'+lg.error_code)
 print('login respond  error_msg:'+lg.error_msg)
 # 显示登陆返回信息
-get_stock_df(stock)
+for stock in stock_list:
+    df = get_stock_df(stock)
+    # analyse df
+
+    # send email
 bs.logout()
